06ffbec20c5a07f7833d7ac577f4bae8d700ae26
Maxime <Maxime@148.60.33.161>
2015-11-19 12:45:05 +0100

    allo

---
+++ Test_Projet/src/Test/Main_Test.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class Main_Test {
+
+	public static void main(String[] args) {
+		//while()//Tant qu'il y a des fichiers à tester
+		//Compare_Test_Oracle(File "test",File "Oracle);
+	}
+
+}
