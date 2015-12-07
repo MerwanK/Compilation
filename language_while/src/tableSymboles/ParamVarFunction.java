3b904257a9173f967a942e8e9935be164b3a2561
MerwanK <merwan.kaf@gmail.com>
2015-12-07 14:42:48 +0100

    tableSymboles

---
+++ language_while/src/tableSymboles/ParamVarFunction.java
@@ -0,0 +1,80 @@
+package tableSymboles;
+
+
+import java.util.HashMap;
+
+public class ParamVarFunction {
+
+	private int nbParamIn;
+	private int nbParamOut;
+	private HashMap<String, String> LocalVars;
+	
+	public ParamVarFunction(int nbIn, int nbOut){
+		nbParamIn = nbIn;
+		nbParamOut = nbOut;
+		LocalVars = new HashMap<String, String>(); 
+	}
+	
+	public ParamVarFunction(){
+		nbParamIn = 0;
+		nbParamOut = 0;
+		LocalVars = new HashMap<String, String>();
+	}
+
+	/**
+	 * @return the nbParamIn
+	 */
+	public int getNbParamIn() {
+		return nbParamIn;
+	}
+
+	/**
+	 * @param nbParamIn the nbParamIn to set
+	 */
+	public void setNbParamIn(int nbParamIn) {
+		this.nbParamIn = nbParamIn;
+	}
+
+	/**
+	 * @return the nbParamOut
+	 */
+	public int getNbParamOut() {
+		return nbParamOut;
+	}
+
+	/**
+	 * @param nbParamOut the nbParamOut to set
+	 */
+	public void setNbParamOut(int nbParamOut) {
+		this.nbParamOut = nbParamOut;
+	}
+
+	/**
+	 * @return the localVars
+	 */
+	public String getValueLocalVars(String nameVarLocal){
+		return LocalVars.get(nameVarLocal);
+	}
+
+	/**
+	 * 
+	 * @param nameVarLocal
+	 * @param value
+	 */
+	
+	public void setLocalVars(String nameVarLocal, String value) {
+		if(!LocalVars.containsKey(nameVarLocal)){
+			LocalVars.put(nameVarLocal, value);
+		}
+	}
+
+	/**
+	 * 
+	 */
+	
+	public String toString() {
+		return "Table des fonctions : [nbInputs =" + nbParamIn + ", nbOutputs =" + nbParamOut + ", Liste des variables locales =" + LocalVars + "]";
+	}
+	
+	
+}
---
+++ language_while/src/tableSymboles/SymbolsTable.java
@@ -0,0 +1,102 @@
+package tableSymboles;
+
+
+import java.util.HashMap;
+
+public class SymbolsTable {
+
+	/**
+	 * Attributes
+	 */
+	
+	private HashMap<String, ParamVarFunction> tableFunctions; 	// Une map avec pour clé le nom de la fonction 
+																// et pour valeur un ensemble d'infos la concernant (cf. classe ParamVarFunction).
+	
+	private HashMap<String, HashMap<String, String>> tableSymbols; 	// Une map avec pour clé le nom de la fonction associée à cette table des symboles 
+																	// et pour valeur une autre map avec pour clé cette fois le nom du symbole et pour valeur ce que vaut ce symbole.
+	/**
+	 * Constructor
+	 */
+	
+	public SymbolsTable(){
+		tableFunctions = new HashMap<String, ParamVarFunction>();
+		tableSymbols = new HashMap<String, HashMap<String, String>>();
+	}
+	
+	/**
+	 * 
+	 * @param nameFunction
+	 */
+	
+	public void putFunction(String nameFunction){
+			ParamVarFunction infosFunc = new ParamVarFunction();
+			tableFunctions.put(nameFunction, infosFunc);
+	}
+	
+	/**
+	 * 
+	 * @param nameFunction
+	 * @param nbIn
+	 * @param nbOut
+	 */
+	
+	public void putFunction(String nameFunction, int nbIn, int nbOut){
+		ParamVarFunction infosFunc = new ParamVarFunction(nbIn, nbOut);
+		tableFunctions.put(nameFunction, infosFunc);
+	}
+	
+	/**
+	 * 
+	 * @param nameFunction
+	 * @return
+	 */
+	
+	public ParamVarFunction getFunction (String nameFunction){
+		return tableFunctions.get(nameFunction);
+	}
+	
+	// A voir si nécessaire de pouvoir changer de nom de fonction ?? (pareil pour var et symb)
+	
+	/**
+	 * 
+	 * @param nameFunction
+	 * @param nameSymb
+	 * @param value
+	 */
+	
+	public void putSymbol(String nameFunction, String nameSymb, String value){
+		HashMap<String, String> symbols = tableSymbols.get(nameFunction);
+		symbols.putIfAbsent(nameSymb, value);
+	}
+	
+	/**
+	 * 
+	 * @param nameFunction
+	 * @param nameSymb
+	 */
+	
+	public void putSymbol(String nameFunction, String nameSymb){
+		HashMap<String, String> symbols = tableSymbols.get(nameFunction);
+		symbols.putIfAbsent(nameSymb, "");
+	}
+	
+	/**
+	 * 
+	 * @param nameFunction
+	 * @return
+	 */
+	
+	public HashMap<String, String> getSymbols (String nameFunction){
+		return tableSymbols.get(nameFunction);
+	}
+
+	/**
+	 * Methode toString()
+	 */
+	
+	public String toString() {
+		return "SymbolsTable [tableFunctions=" + tableFunctions + ", tableSymbols="+ tableSymbols + "]";
+	}
+	
+	
+}
