4b31326ef2dc489d6c36b30117a9f31b3f206539
Maxime <Maxime@Windows>
2016-01-11 14:41:46 +0100

    Backend du nop fonctionnel(sans utilisation de la table des symboles)

--- language_while/src/backEnd/TroisAddVersPython.java
+++ language_while/src/backEnd/TroisAddVersPython.java
@@ -2,9 +2,6 @@ package backEnd;
 
 import java.util.Iterator;
 import java.util.List;
-
-import org.eclipse.xtext.xbase.scoping.batch.ConstructorDelegateScope;
-
 import code3adresses.CodeGenere;
 import code3adresses.Quadruplet;
 
@@ -13,11 +10,20 @@ public class TroisAddVersPython {
 	public static void main (String [] arg){
 		CodeGenere code = new CodeGenere();
 		CodeGenere codeF = new CodeGenere();
+		CodeGenere codeW = new CodeGenere();
+		CodeGenere codeW1 = new CodeGenere();
+		CodeGenere codeW2 = new CodeGenere();
 		codeF.addRead("A");
-		codeF.addNop();
+		codeF.addRead("B");
+		codeW2.addNop();
+		codeW1.addWhile("expr",codeW2);
+		codeW1.addNop();
+		codeW.addWhile("expr",codeW1);
+		codeW.addNop();
+		codeF.addWhile("expr",codeW);
 		codeF.addWrite("B");
 		code.addFonction("f1", codeF);
-		
+
 		System.out.println(codePython(code));
 	}
 
@@ -37,38 +43,51 @@ public class TroisAddVersPython {
 			res += traducteur(currentQuadruplet);
 
 			if(currentQuadruplet.isSousCode()){
-				res += codePython(currentQuadruplet.getOperateur().getCodeGenere(),niveau+1);
+				res += indent(codePython(currentQuadruplet.getOperateur().getCodeGenere(),niveau+1));
 			}
 		}
-		return indent(res,niveau);
+		return res;
 	}
 
 	public static String traducteur(Quadruplet quadruplet){// faire le lien avec la table de symobole
+		String res = "";
+		
 		switch (quadruplet.getOperateur().getOperateur()) 
 		{ 
-		case "fonction": return "fonction f1\n"; 
-		case "read"    : return "read a";
-		case "write"   : return "write b";
-		case "nop"	   : return "pass"; 
-		default		   : return "erreur quadruplet inexistant ";
-		}
-
-	}
-
-	public static String indent(String code, int niveau){
-		String res = "\t";
-		int i;
-		while(niveau!=0){
-			i=0;
-			while (i < code.length()){
-				char current = code.charAt(i);
-				res += current;
-				if(current == 10)
-					res += "\t";
-				i++;
-			}
-			niveau--;
+		case "fonction": List<Quadruplet> listQuadruplet = quadruplet.getOperateur().getCodeGenere().getListQuadruplet(); 
+						 res += "def ";
+						 res += "fonction1 (";
+						 Iterator<Quadruplet> it = listQuadruplet.iterator();
+						 Quadruplet currentQuadruplet = it.next();	
+						 while(currentQuadruplet.getOperateur().getOperateur()=="read"){
+							res += currentQuadruplet.getElement1();
+							Quadruplet nextQuadruplet = it.next();	
+							if (nextQuadruplet.getOperateur().getOperateur()=="read"){
+								res +=", ";
+							}
+							currentQuadruplet = nextQuadruplet;
+						 }
+						 res += ") :\n";break; 
+		case "write"   : res = "return b\n";break;
+		case "nop"	   : res = "pass\n";break; 
+		case "while"   : res = "while expr\n";break;
+		
+		default		   : break;
 		}
 		return res;
+	}
+
+	public static String indent(String code){
+		String res = "\t";
+		int i=0;
+		while (i < code.length()-1){
+			char current = code.charAt(i);
+			char next = code.charAt(i+1);
+			res += current;
+			if(current == 10 && next!=0)
+				res += "\t";
+			i++;
+		}
+		return res + "\n";
 	}
 }
