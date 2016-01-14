33015810d069084f39368586916c7cefcc2dfb12
mastertheif <mastertheif@mastertheif-HP-EliteBook-2560p>
2016-01-14 17:58:51 +0100

    Exec final?

--- language_while/src/exec/Test1.py
+++ language_while/src/exec/Test1.py
@@ -5,10 +5,10 @@ nbarg = 3
 narg = []
 
 for index in range(len(sys.argv)-1):
-	narg.append(translate(sys.argv[index]))
+	narg.append(BinTrees.BinTrees.stringToTree(sys.argv[index]))
 
 while(len(narg) < nbarg):
-	narg.append()
+	narg.append(BinTrees.BinTrees())
 
 def p (var0, var1) :
 	pass
@@ -16,7 +16,7 @@ def p (var0, var1) :
 
 def d (var0, var1, var2) :
 	pass
-	print var1
+	BinTrees.printTree(var1)
 
 if __name__ == '__main__':
 	d(narg[0], narg[1], narg[2])
